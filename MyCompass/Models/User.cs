e9dcb7316d113525cabfee3eb4a5e9c2198ce6a2
Ohad Koren <ohadkoren1997@gmail.com>
2021-11-12 15:32:13 +0200

    ignore create new on users page

--- MyCompass/Models/User.cs
+++ MyCompass/Models/User.cs
@@ -12,15 +12,21 @@ namespace MyCompass.Models
     {
         [Key]
         [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [MinLength(3)]
+        [MaxLength(18)]
+
         public string Password { get; set; }
 
         [EmailAddress]
         public string Email { get; set; }
-
+        
+        [Range(0, 120)]
         public int Age { get; set; }
 
         public UserType Type { get; set; } = UserType.Client;
